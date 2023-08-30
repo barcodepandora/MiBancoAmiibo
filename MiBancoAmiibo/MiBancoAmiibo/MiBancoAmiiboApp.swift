27a9feccf37510e78d5d41d98a21159a40691a22
Juan <jmoreno@novopayment.com>
2023-08-30 11:33:59 -0500

    First and import

---
+++ MiBancoAmiibo/MiBancoAmiibo/MiBancoAmiiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiBancoAmiiboApp.swift
+//  MiBancoAmiibo
+//
+//  Created by Juan Manuel Moreno on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MiBancoAmiiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
